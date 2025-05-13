4b8a66fe63d203e54a85020e0c82a8d65dcf1c46
yashaswi jaiswal <167241629+yashijais5@users.noreply.github.com>
2025-05-13 09:06:44 +0530

    Add files via upload

---
+++ connect_memory_with_llm.py
@@ -0,0 +1,63 @@
+import os
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+# Load environment variables
+load_dotenv()
+
+# Configuration
+HF_TOKEN = os.getenv("HF_TOKEN")
+HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
+
+def load_llm(repo_id):
+    return HuggingFaceEndpoint(
+        repo_id=repo_id,
+        task="text-generation",
+        huggingfacehub_api_token=HF_TOKEN,
+        temperature=0.5,
+        model_kwargs={
+            "max_length": 512
+        }
+    )
+
+CUSTOM_PROMPT_TEMPLATE = """
+Use the pieces of information provided in the context to answer user's question.
+If you dont know the answer, just say that you dont know, dont try to make up an answer. 
+Dont provide anything out of the given context
+
+Context: {context}
+Question: {question}
+
+Start the answer directly. No small talk please.
+"""
+
+def set_custom_prompt(custom_prompt_template):
+    prompt = PromptTemplate(
+        template=custom_prompt_template,
+        input_variables=["context", "question"]
+    )
+    return prompt
+
+# Load Database
+DB_FAISS_PATH = "vectorstore/db_faiss"
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+db = FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
+
+# Create QA chain
+qa_chain = RetrievalQA.from_chain_type(
+    llm=load_llm(HUGGINGFACE_REPO_ID),
+    chain_type="stuff",
+    retriever=db.as_retriever(search_kwargs={'k': 3}),
+    return_source_documents=True,
+    chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
+)
+
+# Run the query
+user_query = input("Write Query Here: ")
+response = qa_chain.invoke({'query': user_query})
+print("RESULT: ", response["result"])
+print("SOURCE DOCUMENTS: ", response["source_documents"])
